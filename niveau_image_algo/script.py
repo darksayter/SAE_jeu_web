1cfac1a5bc16ff769e12ff1a8aea1138b878af58
scipionc <clementduchemin6@gmail.com>
2024-11-21 17:07:28 +0100

    niveau 3/4 finis

--- niveau_image_algo/script.py
+++ niveau_image_algo/script.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import random
 
-#SCRIPT POUR CRYPTER L'IMAGE
+#SCRIPT
 
 input_path = "MOTDEPASSE.png"
 output_path = "image_modifiee.png"
@@ -12,16 +12,21 @@ output_img = Image.new("RGBA", (width, height))
 
 for x in range(width):
     for y in range(height):
+        # Obtenir la valeur du pixel (0 pour noir, 255 pour blanc)
         pixel = img.getpixel((x, y))
         
+        # Générer une couleur aléatoire (R, G, B)
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         
-        if pixel > 127:
+        # Définir l'opacité : 49% pour blanc, 51% pour noir
+        if pixel > 127:  # Considéré comme blanc
             alpha = int(0.49 * 255)
-        else: 
+        else:  # Considéré comme noir
             alpha = int(0.51 * 255)
         
+        # Appliquer le nouveau pixel à l'image de sortie
         output_img.putpixel((x, y), (*color, alpha))
 
+# Sauvegarder l'image modifiée
 output_img.save(output_path)
 print("Image transformée et sauvegardée !")
